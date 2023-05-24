41aa5203f4f03322f311816c5314c64b8823839c
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2023-05-24 10:57:50 +0530

    Added time functions

--- srch_cmp.cpp
+++ srch_cmp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 int BinarySearch(int a[], int start, int end, int item, int iter)
@@ -46,18 +47,27 @@ int LinearSearch(int a[], int n, int item)
 
 int main()
 {
-   int n, i, B, L, a[10]={2, 7, 14, 24, 26, 35, 38, 41, 49, 53};
-   cout<<"\nEnter the element to be searched: ";
-   cin>>n;
-   cout<<"\n\nBinary Search :";
-   B = BinarySearch(a, 0, 9, n, 0);
-   cout<<"\n\nLinear Search :";
-   L = LinearSearch(a, 10, n);
-   if(L > B)
-      cout<<"\n\nBinary search is better for this search.\n";
-   else if(L < B)
-      cout<<"\n\nLinear search is better for this search.\n";
-   else
-      cout<<"\n\nBoth are equally efficient for this search.\n";
-   return 0;
+        clock_t start1, end1, start2, end2;
+        int n, i, B, L, a[10]={2, 7, 14, 24, 26, 35, 38, 41, 49, 53};
+        cout<<"\nEnter the element to be searched: ";
+        cin>>n;
+        cout<<"\n\nBinary Search :";
+        start1 = clock();
+        B = BinarySearch(a, 0, 9, n, 0);
+        end1 = clock();
+        cout << "\nTime taken by binary search: " << (double)(end1-start1)/CLOCKS_PER_SEC << endl;
+        cout<<"\n\nLinear Search :";
+        start2 = clock();
+        L = LinearSearch(a, 10, n);
+        end2 = clock();
+        cout << "\nTime taken by linear search: " << (double)(end2-start2)/CLOCKS_PER_SEC << endl;
+        if(L > B)
+                cout<<"\n\nBinary search is better for this search.\n";
+        else if(L < B)
+                cout<<"\n\nLinear search is better for this search.\n";
+        else
+                cout<<"\n\nBoth are equally efficient for this search.\n";
+
+        return 0;
 }
+
